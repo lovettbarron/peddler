6dbd720e03cf56669c3b7ea4405919875aee5f1a
Andrew Lovett Barron <andrew@relaystudio.com>
2016-07-27 16:41:04 -0400

    Adding views and boilerplate

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+var defaults = require('./keys.js')
+
 app.get('/', function (req, res) {
   
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+  console.log(defaults)
 });
